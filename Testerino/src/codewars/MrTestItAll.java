faf670399c7519d2ca5608d5d66712150c035459
Bresch90 <alexander.bresch@hotmail.com>
2021-10-12 12:05:16 +0200

    Testing with new file. Old TestAll not showing in local repo?

---
+++ Testerino/src/codewars/MrTestItAll.java
@@ -0,0 +1,11 @@
+package codewars;
+
+public class MrTestItAll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//so this is stuffs why not showing up?
+
+	}
+
+}
