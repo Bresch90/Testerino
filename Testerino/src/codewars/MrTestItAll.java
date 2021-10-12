5f81832c481b15af6a098e1a0321244c98640833
Bresch90 <alexander.bresch@hotmail.com>
2021-10-12 12:16:51 +0200

    Last test..maybe

--- Testerino/src/codewars/MrTestItAll.java
+++ Testerino/src/codewars/MrTestItAll.java
@@ -5,7 +5,7 @@ public class MrTestItAll {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//so this is stuffs why not showing up?
-
+		//should merge this and delete new branch
 	}
 
 }
